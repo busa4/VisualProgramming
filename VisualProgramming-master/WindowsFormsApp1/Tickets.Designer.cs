71d3d22cd35502a859297e81d73730f632b55221
IvanFjodorov <fjodorov.ioann@gmail.com>
2023-10-06 16:09:51 +0300

    sa2

---
+++ VisualProgramming-master/WindowsFormsApp1/Tickets.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Tickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.resulta = new System.Windows.Forms.Label();
+            this.resultb = new System.Windows.Forms.Label();
+            this.resultc = new System.Windows.Forms.Label();
+            this.resultt = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(32, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 178);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ticket Sold";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(64, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the number fo tickets sold for each class of seats";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Class A:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Class B:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Class C:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 179);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(301, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(219, 178);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Revenue Generated";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(321, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Class C:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(321, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Class B:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(321, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Class A:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(332, 179);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Total:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // resulta
+            // 
+            this.resulta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resulta.Location = new System.Drawing.Point(382, 58);
+            this.resulta.Name = "resulta";
+            this.resulta.Size = new System.Drawing.Size(79, 25);
+            this.resulta.TabIndex = 13;
+            this.resulta.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.resulta.Click += new System.EventHandler(this.resulta_Click);
+            // 
+            // resultb
+            // 
+            this.resultb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultb.Location = new System.Drawing.Point(382, 95);
+            this.resultb.Name = "resultb";
+            this.resultb.Size = new System.Drawing.Size(79, 25);
+            this.resultb.TabIndex = 14;
+            this.resultb.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // resultc
+            // 
+            this.resultc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultc.Location = new System.Drawing.Point(382, 131);
+            this.resultc.Name = "resultc";
+            this.resultc.Size = new System.Drawing.Size(79, 25);
+            this.resultc.TabIndex = 15;
+            this.resultc.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // resultt
+            // 
+            this.resultt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultt.Location = new System.Drawing.Point(382, 170);
+            this.resultt.Name = "resultt";
+            this.resultt.Size = new System.Drawing.Size(79, 25);
+            this.resultt.TabIndex = 16;
+            this.resultt.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 63);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 63);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(327, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 63);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Exite";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Tickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultt);
+            this.Controls.Add(this.resultc);
+            this.Controls.Add(this.resultb);
+            this.Controls.Add(this.resulta);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Tickets";
+            this.Text = "Tickets";
+            this.Load += new System.EventHandler(this.Tickets_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label resulta;
+        private System.Windows.Forms.Label resultb;
+        private System.Windows.Forms.Label resultc;
+        private System.Windows.Forms.Label resultt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ VisualProgramming-master/WindowsFormsApp1/Tickets.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Tickets : Form
+    {
+        public Tickets()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tickets_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resulta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool b1 = int.TryParse(textBox1.Text, out int classa);
+            bool b2 = int.TryParse(textBox2.Text, out int classb);
+            bool b3 = int.TryParse(textBox3.Text, out int classc);
+
+            if (b1 && b2 && b3 == true)
+            {
+                resulta.Text = (classa * 15).ToString() + " $";
+                resultb.Text = (classb * 12).ToString() + " $";
+                resultc.Text = (classc * 9).ToString() + " $";
+                resultt.Text = (classa * 15 + classb * 12 + classc * 9).ToString() + " $";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resulta.Text = "";
+            resultb.Text = "";
+            resultc.Text = "";
+            resultt.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+        }
+    }
+}
